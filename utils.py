8a2be4e97c4b982cb6b91b1ddde5cf3576b16d35
Emiliyan192 <emg192@gmail.com>
2021-01-19 20:17:14 +0100

    create and plot a confusion matrix

--- utils.py
+++ utils.py
@@ -46,3 +46,25 @@ def save_fig(fig_name, fig_dir, tight_layout=True, fig_extension="png", resoluti
     if tight_layout:
         plt.tight_layout()
     plt.savefig(path, format=fig_extension, dpi=resolution)
+
+
+def confusion_matrix(model, y_pred, y_true, fig_size=10):
+
+stacked = torch.stack((y_true, y_pred), dim=1)
+confusion_matrix = torch.zeros(25, 25, dtype=torch.int64)
+
+for p in stacked:
+    tl, pl = p.tolist()
+    confusion_matrix[tl, pl] = confusion_matrix[tl, pl] + 1
+
+letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y']
+
+#delete the row coresponding to the j'th letter since it is out of the dataset because it involves motion
+confusion_matrix = np.delete(confusion_matrix, 9, axis=0)
+confusion_matrix = np.delete(confusion_matrix, 9, axis=1)
+
+# create the plot
+plt.figure(figsize = (fig_size,fig_size))
+ax = sns.heatmap(confusion_matrix, cmap= "Blues", linecolor = 'black' , linewidth = 0, annot = True, fmt='', xticklabels = letters, yticklabels = letters)
+ax.set(xlabel='Classified as', ylabel='True label')
+plt.show()
