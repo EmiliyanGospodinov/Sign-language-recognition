22759f85f5c42b850d951d5859a32c9828ce3683
Emiliyan192 <emg192@gmail.com>
2021-01-21 19:38:54 +0100

    add a method for inference with a trained model

---
+++ infer.py
@@ -0,0 +1,49 @@
+import os
+import torch
+from torchvision import transforms
+from PIL import Image
+import argparse
+
+
+map_characters = np.array(['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y'])
+
+def get_args_parser():
+    parser = argparse.ArgumentParser(description="Model inference")
+    parser.add_argument("model", type=str, help="Model path")
+    parser.add_argument("img_dir", type=str, help="image path")
+    return parser
+
+
+def infer(model, img_dir):
+
+    model.eval()
+
+    transform = transforms.Compose([
+        transforms.Grayscale(),
+        transforms.Resize((28, 28)),
+        transforms.ToTensor(),
+    ])
+
+    data = []
+    for filename in os.listdir(img_dir):
+        image = transform(Image.open(img_dir + '/' + filename))
+        data.append(image)
+    data = torch.stack(data, 0)
+
+    outputs = model(data.to(device).reshape(-1, 1, 28, 28))
+    _, preds = torch.max(outputs, 1)
+
+    print("Input images corresponds to character: ", map_characters[preds.cpu().numpy()])
+
+
+if __name__ == "__main__":
+    arg_parser = get_args_parser()
+    args = arg_parser.parse_args()
+
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    print(f"Running on device: {device}")
+
+    model = torch.load(args.model)
+
+    infer(model, args.img_dir)
+
