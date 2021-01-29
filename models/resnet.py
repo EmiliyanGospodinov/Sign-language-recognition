9f9b7b9e726997a97703a9fd3ec7836d4b7f9e36
Haobin Tan <develop.haobin.tan@gmail.com>
2021-01-29 23:13:16 +0100

    Remove unnecessary main function

--- models/resnet.py
+++ models/resnet.py
@@ -1,7 +1,6 @@
-import torch
-from torch import Tensor
 import torch.nn as nn
 from torchvision import models
+
 
 def initialize_model():
     """
@@ -11,17 +10,11 @@ def initialize_model():
 
     Returns
     -------
-    Resnet18: Cnn model     
+    Resnet18: Cnn model
     """
     model = models.resnet18(pretrained=False)
     model.conv1 = nn.Conv2d(1, 64, kernel_size=7, stride=2, padding=3, bias=False)
     num_ftrs = model.fc.in_features
     model.fc = nn.Linear(num_ftrs, 25)
-    
+
     return model
-
-
-if __name__ == "__main__":
-    model = initialize_model()
-    torch.save(model, "../saved_models/resnet.pt")
-    print("Model created and saved in: " + " ../saved_models/resnet.pt")
--- models/squeezenet.py
+++ models/squeezenet.py
@@ -1,7 +1,6 @@
-import torch
-from torch import Tensor
 import torch.nn as nn
 from torchvision import models
+
 
 def initialize_model():
     """
@@ -11,16 +10,10 @@ def initialize_model():
 
     Returns
     -------
-    Squeezenet: Cnn model     
+    Squeezenet: Cnn model
     """
     model = models.squeezenet1_0(pretrained=False)
     model.features[0] = nn.Conv2d(1, 96, kernel_size=7, stride=2)
-    model.classifier[1] = nn.Conv2d(512, 25, kernel_size=(1,1), stride=(1,1))
-    
+    model.classifier[1] = nn.Conv2d(512, 25, kernel_size=(1, 1), stride=(1, 1))
+
     return model
-
-
-if __name__ == "__main__":
-    model = initialize_model()
-    torch.save(model, "../saved_models/squeezenet.pt")
-    print("Model created and saved in: " + " ../saved_models/squeezenet.pt")
